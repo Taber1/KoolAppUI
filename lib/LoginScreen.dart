f05649567b2cd57f1ea536b6771e27d292cb4735
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-08 03:27:10 +0500

    Login Screen Design | issue resolved

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -11,10 +11,8 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Container(
+      body: SingleChildScrollView(
         child: Stack(
-          fit: StackFit.passthrough,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -25,123 +23,127 @@ class _LoginScreenState extends State<LoginScreen> {
               height: MediaQuery.of(context).size.height * 0.4,
               width: MediaQuery.of(context).size.width,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 55,
+            Padding(
+              padding: EdgeInsets.only(top: 120, left: 10, right: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 55,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/logo-kool-Jaune-png.png'))),
+                      )),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Material(
+                    elevation: 10,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
                     child: Container(
                       decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(
-                                  'assets/images/logo-kool-Jaune-png.png'))),
-                    )),
-                SizedBox(
-                  height: 50,
-                ),
-                Material(
-                  elevation: 10,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          TextField(
-                              decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                                borderRadius: BorderRadius.circular(5)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                            labelText: 'Email',
-                            hintText: "Enter Email",
-                            labelStyle: TextStyle(color: Color(0xffffd400)),
-                          )),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          TextField(
-                              decoration: InputDecoration(
-                                  // enabledBorder: OutlineInputBorder(
-                                  //     borderSide:
-                                  //         BorderSide(color: Colors.grey),
-                                  //     borderRadius: BorderRadius.circular(5)),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(5)),
-                                  labelText: 'Password',
-                                  hintText: "******",
-                                  labelStyle:
-                                      TextStyle(color: Color(0xffffd400)),
-                                  suffixIcon: Icon(Icons.remove_red_eye))),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          RaisedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomeScreen()));
-                            },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0)),
-                            padding: EdgeInsets.all(0.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Color(0xffffd400),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(50.0)),
-                              ),
-                              height: MediaQuery.of(context).size.height * 0.06,
-                              alignment: Alignment.center,
-                              child: const Text(
-                                'LOGIN',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 17),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextField(
+                                decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(5)),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              labelText: 'Email',
+                              hintText: "Enter Email",
+                              labelStyle: TextStyle(color: Color(0xffffd400)),
+                            )),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextField(
+                                decoration: InputDecoration(
+                                    // enabledBorder: OutlineInputBorder(
+                                    //     borderSide:
+                                    //         BorderSide(color: Colors.grey),
+                                    //     borderRadius: BorderRadius.circular(5)),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5)),
+                                    labelText: 'Password',
+                                    hintText: "******",
+                                    labelStyle:
+                                        TextStyle(color: Color(0xffffd400)),
+                                    suffixIcon: Icon(Icons.remove_red_eye))),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            RaisedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomeScreen()));
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0)),
+                              padding: EdgeInsets.all(0.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xffffd400),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(50.0)),
+                                ),
+                                height:
+                                    MediaQuery.of(context).size.height * 0.06,
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'LOGIN',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeScreen()));
-                      },
-                      child: Text("Forget Password"),
-                    )
-                  ],
-                )
-              ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen()));
+                        },
+                        child: Text("Forget Password"),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
           clipBehavior: Clip.none,
         ),
       ),
-    ));
+    );
   }
 }
