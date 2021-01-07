2e0ec7e32420f4751765fb83a0c349296c91ead1
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-08 03:16:57 +0500

    Login Screen Design

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:kool_app_ui/HomeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,21 +12,22 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)),
-                color: Color(0xffffd400)),
-            height: MediaQuery.of(context).size.height * 0.4,
-            width: MediaQuery.of(context).size.width,
-          ),
-          Positioned(
-            top: 100,
-            left: 20,
-            child: Column(
+      child: Container(
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40)),
+                  color: Color(0xffffd400)),
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 CircleAvatar(
                     backgroundColor: Colors.white,
@@ -52,6 +54,8 @@ class _LoginScreenState extends State<LoginScreen> {
                       padding: const EdgeInsets.only(
                           left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           TextField(
                               decoration: InputDecoration(
@@ -60,32 +64,36 @@ class _LoginScreenState extends State<LoginScreen> {
                                 borderRadius: BorderRadius.circular(5)),
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(5)),
-                            hintText: 'Email',
-                            hintStyle: TextStyle(
-                                fontSize: 18, color: Color(0xffffd400)),
+                            labelText: 'Email',
+                            hintText: "Enter Email",
+                            labelStyle: TextStyle(color: Color(0xffffd400)),
                           )),
                           SizedBox(
                             height: 20,
                           ),
                           TextField(
                               decoration: InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide:
-                                          BorderSide(color: Colors.grey),
-                                      borderRadius: BorderRadius.circular(5)),
+                                  // enabledBorder: OutlineInputBorder(
+                                  //     borderSide:
+                                  //         BorderSide(color: Colors.grey),
+                                  //     borderRadius: BorderRadius.circular(5)),
                                   border: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular(5)),
-                                  hintText: 'Password',
-                                  hintStyle: TextStyle(
-                                      fontSize: 18, color: Color(0xffffd400)),
-                                  suffixIcon: Icon(
-                                    Icons.mail,
-                                  ))),
+                                  labelText: 'Password',
+                                  hintText: "******",
+                                  labelStyle:
+                                      TextStyle(color: Color(0xffffd400)),
+                                  suffixIcon: Icon(Icons.remove_red_eye))),
                           SizedBox(
                             height: 30,
                           ),
                           RaisedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeScreen()));
+                            },
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(50.0)),
                             padding: EdgeInsets.all(0.0),
@@ -93,7 +101,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               decoration: BoxDecoration(
                                 color: Color(0xffffd400),
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(80.0)),
+                                    BorderRadius.all(Radius.circular(50.0)),
                               ),
                               height: MediaQuery.of(context).size.height * 0.06,
                               alignment: Alignment.center,
@@ -112,11 +120,27 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      },
+                      child: Text("Forget Password"),
+                    )
+                  ],
+                )
               ],
             ),
-          )
-        ],
-        clipBehavior: Clip.none,
+          ],
+          clipBehavior: Clip.none,
+        ),
       ),
     ));
   }
