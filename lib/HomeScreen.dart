4bdc778ff3bbf298df613053867b7ba110f7d8b7
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-09 02:45:26 +0500

    Home Screen new design

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -50,27 +50,77 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-          child: Center(
-            child: Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                InkWell(
-                  child: Icon(
-                    FontAwesomeIcons.shoppingCart,
-                    size: 30,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen()));
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0)),
+                    padding: EdgeInsets.all(0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'A emporter',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                    ),
                   ),
-                  onTap: () {},
-                ),
-                CircleAvatar(
-                  radius: 5,
-                  backgroundColor: Color(0xffffd400),
-                  child: Text(
-                    "0",
-                    style: TextStyle(color: Colors.white),
+                  SizedBox(
+                    width: 10,
                   ),
-                )
-              ],
-            ),
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen()));
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0)),
+                    padding: EdgeInsets.all(0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffffd400),
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Livraison',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
