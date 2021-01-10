b0cc63a9a7a39f8dcb13525f94ea189d75d59a74
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-10 20:24:27 +0500

    NotificationScreen | Completed

--- lib/NotificationScreen.dart
+++ lib/NotificationScreen.dart
@@ -26,6 +26,265 @@ class _NotificationScreenState extends State<NotificationScreen> {
               icon: Icon(FontAwesomeIcons.shoppingCart), onPressed: () {})
         ],
       ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20, left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Text(
+                "Les notifications",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.radio_button_checked,
+                    color: Color(0xffffd400), size: 22),
+                SizedBox(
+                  width: 8,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Le statut de la commande a change",
+                        style: TextStyle(
+                            color: Color(0xffffd400),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Votre commande #163 est prete et en attente de livreur",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Benkay Sushi",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "2020-12-28 - 15:58",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.radio_button_checked, color: Colors.grey, size: 22),
+                SizedBox(
+                  width: 8,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Le statut de la commande a change",
+                        style: TextStyle(
+                            color: Color(0xffffd400),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Votre commande #163 est prete et en attente de livreur",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Benkay Sushi",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "2020-12-28 - 15:58",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.radio_button_checked, color: Colors.grey, size: 22),
+                SizedBox(
+                  width: 8,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Le statut de la commande a change",
+                        style: TextStyle(
+                            color: Color(0xffffd400),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Votre commande #163 est prete et en attente de livreur",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Benkay Sushi",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "2020-12-28 - 15:58",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.radio_button_checked, color: Colors.grey, size: 22),
+                SizedBox(
+                  width: 8,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Le statut de la commande a change",
+                        style: TextStyle(
+                            color: Color(0xffffd400),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Votre commande #163 est prete et en attente de livreur",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Benkay Sushi",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "2020-12-28 - 15:58",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.radio_button_checked, color: Colors.grey, size: 22),
+                SizedBox(
+                  width: 8,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Le statut de la commande a change",
+                        style: TextStyle(
+                            color: Color(0xffffd400),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Votre commande #163 est prete et en attente de livreur",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Benkay Sushi",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "2020-12-28 - 15:58",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
