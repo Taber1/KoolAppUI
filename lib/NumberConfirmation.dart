4103071e90bdbd206994c110995d156fa776af34
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-10 23:38:59 +0500

    Confirmation Designing

--- lib/NumberConfirmation.dart
+++ lib/NumberConfirmation.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'HomeScreen.dart';
+
 class NumberSuccessful extends StatefulWidget {
   @override
   _NumberSuccessfulState createState() => _NumberSuccessfulState();
@@ -9,28 +11,53 @@ class _NumberSuccessfulState extends State<NumberSuccessful> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: RaisedButton(
-        color: Colors.transparent,
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => NumberScreen()));
-        },
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
-        padding: EdgeInsets.all(0.0),
-        child: Container(
-          decoration: BoxDecoration(
+      body: Center(
+        child: Wrap(alignment: WrapAlignment.center, children: [
+          Icon(
+            Icons.check_circle,
             color: Color(0xffffd400),
-            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            size: 200,
           ),
-          height: MediaQuery.of(context).size.height * 0.06,
-          width: MediaQuery.of(context).size.width * 0.9,
-          alignment: Alignment.center,
-          child: const Text(
-            "Page d'acceuil",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "votre numero a verivie avec succes",
+            style: TextStyle(color: Colors.grey, fontSize: 20),
+          )
+        ]),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            elevation: 0,
+            color: Colors.white,
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()));
+            },
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            padding: EdgeInsets.all(0.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xffffd400),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              height: MediaQuery.of(context).size.height * 0.06,
+              width: MediaQuery.of(context).size.width * 0.9,
+              alignment: Alignment.center,
+              child: const Text(
+                "Page d'acceuil",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              ),
+            ),
           ),
         ),
       ),
--- lib/NumberScreen.dart
+++ lib/NumberScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kool_app_ui/otpScreen.dart';
+import 'package:kool_app_ui/OtpScreen.dart';
 
 class NumberScreen extends StatefulWidget {
   @override
