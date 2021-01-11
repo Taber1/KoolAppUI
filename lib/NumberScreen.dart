9bb224c831c0b09ee41879dbb8c77b48f774cab1
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-11 15:52:12 +0500

    Number Screen

--- lib/NumberScreen.dart
+++ lib/NumberScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kool_app_ui/OtpScreen.dart';
 
 class NumberScreen extends StatefulWidget {
@@ -21,32 +22,68 @@ class _NumberScreenState extends State<NumberScreen> {
         ),
       ),
       body: Center(
-        child: RaisedButton(
-          color: Colors.transparent,
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => OtpScreen()));
-          },
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          padding: EdgeInsets.all(0.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xffffd400),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            height: MediaQuery.of(context).size.height * 0.06,
-            width: MediaQuery.of(context).size.width * 0.8,
-            alignment: Alignment.center,
-            child: const Text(
-              'Valider',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Veuillez nous indiquer votre numero de\ntelelphone un code de verification vous sera\nenvoye",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17),
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
             ),
-          ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10),
+              child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    prefixText: '+216 ',
+                    prefixStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    prefixIcon: Icon(Icons.flag),
+                    labelStyle: TextStyle(
+                        color: Color(0xffffd400), fontWeight: FontWeight.bold),
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.transparent,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => OtpScreen()));
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.all(0.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xffffd400),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width * 0.8,
+                alignment: Alignment.center,
+                child: const Text(
+                  'Valider',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
