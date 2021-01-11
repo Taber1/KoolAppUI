722f3e3d244cab924d488f69728dae0f42a5f871
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-12 04:51:20 +0500

    Items List

---
+++ lib/itemList.dart
@@ -0,0 +1,67 @@
+var item = [
+  {
+    "name": 'Pasta Box',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/03/16/21/11/noodles-2150272__340.jpg'
+  },
+  {
+    "name": 'Chicken',
+    "picture":
+        'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg'
+  },
+  {
+    "name": 'Spaghetti',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/11/08/22/18/spaghetti-2931846__340.jpg'
+  },
+  {
+    "name": 'Hamburger',
+    "picture":
+        'https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706__340.jpg'
+  },
+  {
+    "name": 'Pizza',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__340.jpg'
+  },
+  {
+    "name": 'Italian Pizza',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272__340.jpg'
+  },
+  {
+    "name": 'Corn',
+    "picture":
+        'https://cdn.pixabay.com/photo/2018/09/08/18/47/corn-3663086__340.jpg'
+  },
+  {
+    "name": 'Sandwich',
+    "picture":
+        'https://cdn.pixabay.com/photo/2015/03/28/10/21/diet-695723__340.jpg'
+  },
+  {
+    "name": 'Bread Rolls',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/05/15/01/18/baked-2313462__340.jpg'
+  },
+  {
+    "name": 'Bread Ciabata',
+    "picture":
+        'https://cdn.pixabay.com/photo/2017/05/15/01/18/baked-2313462__340.jpg'
+  },
+  {
+    "name": 'Christmas Cookies',
+    "picture":
+        'https://cdn.pixabay.com/photo/2016/12/06/15/26/christmas-cookies-1886760__340.jpg'
+  },
+  {
+    "name": 'Lasagna',
+    "picture":
+        'https://cdn.pixabay.com/photo/2016/12/11/22/41/lasagna-1900529__340.jpg'
+  },
+  {
+    "name": 'Desert',
+    "picture":
+        'https://cdn.pixabay.com/photo/2014/07/21/23/00/dessert-398966__340.jpg'
+  },
+];
