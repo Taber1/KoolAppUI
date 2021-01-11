005e7a38d1e013eac3d99aa0ecbd45a0c6602d3f
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-11 23:55:00 +0500

    Parametres Screen | Round bottom appbar

--- lib/NumberConfirmation.dart
+++ lib/NumberConfirmation.dart
@@ -30,35 +30,52 @@ class _NumberSuccessfulState extends State<NumberSuccessful> {
               )
             ]),
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: RaisedButton(
-            elevation: 0,
-            color: Colors.white,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()));
-            },
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            padding: EdgeInsets.all(0.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xffffd400),
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
               ),
-              height: MediaQuery.of(context).size.height * 0.06,
-              width: MediaQuery.of(context).size.width * 0.9,
-              alignment: Alignment.center,
-              child: const Text(
-                "Page d'acceuil",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17),
+            ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomAppBar(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                elevation: 0,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                padding: EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffffd400),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Page d'acceuil",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ),
+                ),
               ),
             ),
           ),
