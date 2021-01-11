fbeab733f1e39212189108d0e5cc8bb55d4bc972
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-12 04:25:28 +0500

    Finishing | NoItemPanier Screen

--- lib/NoItemPanierScreen.dart
+++ lib/NoItemPanierScreen.dart
@@ -23,54 +23,67 @@ class _NoItemPanierScreenState extends State<NoItemPanierScreen> {
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              FontAwesomeIcons.shoppingCart,
-              size: 130,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/circle.png',
+              alignment: Alignment.bottomRight,
+              color: Colors.grey[200],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Votre panier est vide",
-              style: TextStyle(color: Colors.grey, fontSize: 25),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              color: Colors.transparent,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => PanierScreen()));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)),
-              padding: EdgeInsets.all(0.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xffffd400),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  FontAwesomeIcons.shoppingCart,
+                  size: 130,
                 ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.45,
-                alignment: Alignment.center,
-                child: const Text(
-                  'Commansez a explorer',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
+                Text(
+                  "Votre panier est vide",
+                  style: TextStyle(color: Colors.grey, fontSize: 25),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  color: Colors.transparent,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PanierScreen()));
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  padding: EdgeInsets.all(0.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xffffd400),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Commansez a explorer',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
