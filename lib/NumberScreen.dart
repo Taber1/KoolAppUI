43b1d7bdb325394172b06ee930c93f4e64fa2426
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-12 04:03:55 +0500

    Finishing | Number Screen

--- lib/NumberScreen.dart
+++ lib/NumberScreen.dart
@@ -16,87 +16,98 @@ class _NumberScreenState extends State<NumberScreen> {
         centerTitle: true,
         automaticallyImplyLeading: false,
         title: Image.asset(
-          'assets/images/logo-kool-Jaune-png.png',
+          'assets/images/kool_black.png',
           color: Colors.black,
-          height: 65,
+          height: 40,
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Veuillez nous indiquer votre numero de\ntelelphone un code de verification vous sera\nenvoye",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/circle.png',
+              alignment: Alignment.bottomRight,
             ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10),
-              child: TextField(
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.flag),
-                          SizedBox(
-                            width: 5,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Veuillez nous indiquer votre numero de telelphone un code de verification vous sera envoye",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.flag),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "+216",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
                           ),
-                          Text(
-                            "+216",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
+                        ),
+                        labelStyle: TextStyle(
+                            color: Color(0xffffd400),
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    color: Colors.transparent,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => OtpScreen()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    padding: EdgeInsets.all(0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 5, color: Color(0xffffd400)),
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.06,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Valider',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
                       ),
                     ),
-                    labelStyle: TextStyle(
-                        color: Color(0xffffd400), fontWeight: FontWeight.bold),
-                  )),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              color: Colors.transparent,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => OtpScreen()));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)),
-              padding: EdgeInsets.all(0.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xffffd400),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.8,
-                alignment: Alignment.center,
-                child: const Text(
-                  'Valider',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17),
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
