5512ff4404a53b0fe92c7ec9c8c48c29f1a19f56
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-11 16:16:19 +0500

    Number Screen

--- lib/otpScreen.dart
+++ lib/otpScreen.dart
@@ -23,32 +23,43 @@ class _OtpScreenState extends State<OtpScreen> {
         ),
       ),
       body: Center(
-        child: RaisedButton(
-          color: Colors.transparent,
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => NumberSuccessful()));
-          },
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          padding: EdgeInsets.all(0.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xffffd400),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Code?",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            height: MediaQuery.of(context).size.height * 0.06,
-            width: MediaQuery.of(context).size.width * 0.8,
-            alignment: Alignment.center,
-            child: const Text(
-              'Valider',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17),
+            RaisedButton(
+              color: Colors.transparent,
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => NumberSuccessful()));
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.all(0.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xffffd400),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width * 0.8,
+                alignment: Alignment.center,
+                child: const Text(
+                  'Valider',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17),
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
