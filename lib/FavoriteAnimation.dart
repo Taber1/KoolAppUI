04b2f7f2d931437d7bd1c216ec153784b1f92ab9
Taber Bin Zameer <tbzameer@gmail.com>
2021-01-13 23:29:35 +0500

    Favorite button animation | Pimp Button

--- lib/FavoriteAnimation.dart
+++ lib/FavoriteAnimation.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class ParticleModel {
@@ -35,5 +36,88 @@ class ParticleModel {
     if (animationProgress.progress(time) == 1.0) {
       restart(time: time);
     }
+  }
+}
+
+class ParticlePainter extends CustomPainter {
+  List<ParticleModel> particles;
+  Duration time;
+
+  ParticlePainter(this.particles, this.time);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.yellow.withAlpha(50);
+
+    particles.forEach((particle) {
+      var progress = particle.animationProgress.progress(time);
+      final animation = particle.tween.transform(progress);
+      final position =
+          Offset(animation["x"] * size.width, animation["y"] * size.height);
+      canvas.drawCircle(position, size.width * 0.2 * particle.size, paint);
+
+      Paint paint1 = Paint();
+      paint1
+        ..color = Colors.yellow[100]
+        ..style = PaintingStyle.fill
+        ..strokeWidth = 0;
+
+      double width = size.width;
+      double height = size.height;
+
+      Path path = Path();
+      path.moveTo(0.5 * width, height * 0.35);
+      path.cubicTo(0.2 * width, height * 0.1, -0.25 * width, height * 0.6,
+          0.5 * width, height);
+      path.moveTo(0.5 * width, height * 0.35);
+      path.cubicTo(0.8 * width, height * 0.1, 1.25 * width, height * 0.6,
+          0.5 * width, height);
+
+      canvas.drawPath(path, paint1);
+    });
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class HeartWidget extends StatefulWidget {
+  @override
+  _HeartWidgetState createState() => _HeartWidgetState();
+}
+
+class _HeartWidgetState extends State<HeartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Testing'),
+      ),
+      body: Center(
+        child: CustomPaint(
+          size: Size(70, 80),
+          painter: HeartPainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class HeartPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+
+    // Paint paint = Paint();
+    // paint
+    //   ..color = Colors.black
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeCap = StrokeCap.round
+    //   ..strokeWidth = 6;
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
